68e7d816eb33b9e6d0d86aae2a6452232c991476
jrkinouani <kinouani@gmail.com>
2017-02-17 16:25:19 +0400

    hotfix

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  
+
 root "static_pages#home"
+get "/inscription" => "static_pages#inscription"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
